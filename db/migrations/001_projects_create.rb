f35be62399beedeb7c1729799ca047e20cca9d82
Robert-Rino <a901002666@gmail.com>
2016-04-14 18:18:44 +0800

    change db schema

--- db/migrations/001_projects_create.rb
+++ db/migrations/001_projects_create.rb
@@ -4,8 +4,9 @@ Sequel.migration do
   change do
     create_table(:projects) do
       primary_key :id
-      String :name, unique: true, null: false
-      String :repo_url, unique: true
+      String :file_extension
+      String :description
+      String :remark
     end
   end
 end
